43efdd3a544d9c1424c002c30145dd9519b66761
Lukasz Milewski <lmmilewski@gmail.com>
2011-03-16 03:56:12 +0100

    add shared_ptr factory to perfect_forwarding example

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ CC=g++-mp-4.5
 #CC=g++
 CPPFLAGS=-O2 -W -Wall -pedantic -std=c++0x
 CPP=$(wildcard *.cpp)
-ALL=binding rvalue_lvalue rvo
+ALL=binding rvalue_lvalue rvo perfect_forwarding
 
 all: $(ALL)
 
--- perfect_forwarding.cpp
+++ perfect_forwarding.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <memory>
 
 struct A {
     int value;
@@ -16,10 +17,11 @@ struct A {
 
     A& operator=(const A& /* rhs */) {
         std::cout << "copying (operator)\n";
+        return *this;
     }
 
     A& operator=(A&& /* rhs */) {
-
+        return *this;
     }
 };
 
@@ -36,14 +38,59 @@ void foo(T&& t) {
     bar(std::forward<T>(t));
 }
 
-void baz(A&& t) {
-    bar(t);
+void baz(A&& t) { // t is lvalue
+    bar(t); // no way to perfect forward (call corresponding bar overload)
+}
+
+
+
+template <typename T, typename A>
+inline
+std::shared_ptr<T>
+factory(A&& a) {
+    return std::shared_ptr<T>(new T(std::forward<A>(a)));
+}
+
+template <typename T, typename A>
+inline
+std::shared_ptr<T>
+factory_noforward(A&& a) {
+    return std::shared_ptr<T>(new T(a));
+}
+
+struct B {
+    B(int&) {
+        std::cout << "B(int&)\n";
+    }
+
+    B(int&&) {
+        std::cout << "B(int&&)\n";
+    }
+};
+
+int make_int() {
+    return 0;
 }
 
 int main() {
-    A x;
-    foo(x); // x is deduced as A& (lvalue ref)
-    foo(A()); // A() is deduced as A
-    baz(x);
-    baz(A());
+    A x; // lvalue
+
+    bar(x);  // calls bar(A&)
+    bar(A()); // calls bar(A&&)
+
+    foo(x); // x is deduced as A& (lvalue ref) // bar(A&) is called
+    foo(A()); // A() is deduced as A // bar(A&&) is called
+
+    // baz(x); // error: invalid initialization of reference of type 'A&&' from expression of type 'A'
+    baz(std::move(x)); // bar(A&) is called
+    baz(A()); // bar(A&) is called
+
+    int i = 0;
+    factory<B>(i); // call B&
+    factory<B>(make_int()); // call B&&
+    factory_noforward<B>(i); // call B&
+    factory_noforward<B>(make_int()); // call B&
 }
+
+
+
