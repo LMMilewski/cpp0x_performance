05765d555e9490be751d2f0156fca07c9c3dbde3
Lukasz Milewski <lmmilewski@gmail.com>
2011-03-17 07:58:33 +0100

    add speed example for PODs

---
+++ pod_memset_loop.cpp
@@ -0,0 +1,19 @@
+// use -DUSE_MEMSET to compile memset version
+
+#include <cassert>
+#include <cstring>
+
+struct A {
+    int x;
+    char y;
+};
+
+int main() {
+    const int n = 1000000;
+    A a[n];
+    volatile int x = 0;
+    for (int j = 0; j < 1000; ++j) {
+        }
+#endif
+    }
+}
